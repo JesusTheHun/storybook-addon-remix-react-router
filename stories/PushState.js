fd80bdaccd65cee041a2276df0912b5296a18cbb
Jonathan Massuchetti <jonathan.massuchetti@dappit.fr>
2022-07-29 15:20:26 +0200

    test: add stories to test location state

---
+++ stories/PushState.js
@@ -0,0 +1,23 @@
+import React, {useEffect} from 'react';
+import {useLocation, useSearchParams} from 'react-router-dom';
+
+export const PushState = () => {
+
+    const location = useLocation();
+
+    const [searchParams, setSearchParams] = useSearchParams();
+    const sort = searchParams.get('sort');
+
+    // Enforce a default `sort` using deep linking
+    useEffect(() => {
+        if (sort === null) {
+            const defaultSearchParams = new URLSearchParams({ sort: "date" });
+            setSearchParams(defaultSearchParams, { state: "defaultSort" });
+        }
+    }, [sort]);
+
+	return <div>
+        Sort : {sort} <br/>
+        State : {JSON.stringify(location.state)}
+	</div>
+}
