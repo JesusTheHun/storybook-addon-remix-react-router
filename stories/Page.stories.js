9bacd2716a6f8771ad1399504efc095d9ff6af7d
Jonathan Massuchetti <jonathan.massuchetti@dappit.fr>
2022-08-02 16:44:44 +0200

    test: add basic nested routing stories

--- stories/Page.stories.js
+++ stories/Page.stories.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 import { Page } from './Page';
 import * as HeaderStories from './Header.stories';
+import {withRouter} from '../src';
 
 export default {
   title: 'Example/Page',
   component: Page,
+  decorators: [withRouter],
 };
 
 const Template = (args) => <Page {...args} />;
@@ -18,4 +20,31 @@ LoggedIn.args = {
 export const LoggedOut = Template.bind({});
 LoggedOut.args = {
   ...HeaderStories.LoggedOut.args,
+};
+
+export const NestedRouteStaticSubpath = Template.bind({});
+NestedRouteStaticSubpath.parameters = {
+  reactRouter: {
+    routePath: '/staticsubpath',
+  }
+};
+
+export const NestedRouteDynamicSubpath = Template.bind({});
+NestedRouteDynamicSubpath.parameters = {
+  reactRouter: {
+    routePath: '/abcdef',
+    searchParams: {
+      foo: "bar",
+    },
+  }
+};
+
+export const WeirdRouteParamValue = Template.bind({});
+WeirdRouteParamValue.parameters = {
+  reactRouter: {
+    routePath: '/:someParam',
+    routeParams: {
+      someParam: ':someValue',
+    }
+  }
 };
