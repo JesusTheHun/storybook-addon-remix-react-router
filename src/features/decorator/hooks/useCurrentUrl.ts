f5af48532a75b7d6e10a6bd1b3599b5e4faca121
Jonathan MASSUCHETTI <jonathan.massuchetti@dappit.fr>
2023-08-22 10:59:56 +0200

    fix(logs): missing question mark before query string

--- src/features/decorator/hooks/useCurrentUrl.ts
+++ src/features/decorator/hooks/useCurrentUrl.ts
@@ -2,5 +2,5 @@ import { useLocation } from 'react-router-dom';
 
 export const useCurrentUrl = () => {
   const location = useLocation();
-  return `${location.pathname}${location.search}${location.hash}`;
+  return `${location.pathname}?${location.search}${location.hash}`;
 };
