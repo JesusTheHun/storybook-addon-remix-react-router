d1f287023c6705645b2f63b07969e3222156b689
Jonathan MASSUCHETTI <jonathan.massuchetti@dappit.fr>
2024-12-10 16:33:49 +0100

    test: remove remix stubs

--- tests/reactRouterV7/setupTests.ts
+++ tests/reactRouterV7/setupTests.ts
@@ -1,12 +1,7 @@
-import { beforeEach, vi } from 'vitest';
+import { beforeEach } from 'vitest';
 import '@testing-library/jest-dom';
-import { fetch, Request, Response } from '@remix-run/web-fetch';
 import { mockLocalStorage } from './test-utils';
 
 beforeEach(() => {
   mockLocalStorage();
-
-  vi.stubGlobal('fetch', fetch);
-  vi.stubGlobal('Request', Request);
-  vi.stubGlobal('Response', Response);
 });
